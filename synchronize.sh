e8fb399aff7710c5874ba2571922b5629c1c9ab6
Ole Manshardt <mail@manshardt.de>
2025-12-12 08:55:11 +0100

    Updated 'synchronize.sh' to switch between relacing and adding audio

--- synchronize.sh
+++ synchronize.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+ADD_OPTION=$1
+
+
 echo "START extracting audio from video files"
 ./01_process_videos.sh
 echo "START correlating extracted audio with audio recordings."
 ./02_correlate_audio.sh
 echo "START adding audio from audio recording to video files"
-./03_process_merge.sh
+if [ "$ADD_OPTION" = "--add" ]; then
+      ./03_process_merge.sh --add
+else
+      ./03_process_merge.sh
+fi
 
 echo "DONE, ENJOY"
